8b974ee31fd5e293bfbb54ddb70b3a955d9e4bc4
basilspice <basil.breton@gmail.com>
2023-12-21 13:36:54 -0800

    finished listing reservation component, reservations working

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,12 +1,14 @@
 "use client";
+
 import L from "leaflet";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-//@ts-ignore
+// @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
@@ -18,6 +20,10 @@ interface MapProps {
   center?: number[];
 }
 
+const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
 const Map: React.FC<MapProps> = ({ center }) => {
   return (
     <MapContainer
@@ -26,15 +32,8 @@ const Map: React.FC<MapProps> = ({ center }) => {
       scrollWheelZoom={false}
       className="h-[35vh] rounded-lg"
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {center && (
-        <Marker
-        position={center as L.LatLngExpression}
-        />
-      )}
+      <TileLayer url={url} attribution={attribution} />
+      {center && <Marker position={center as L.LatLngExpression} />}
     </MapContainer>
   );
 };
