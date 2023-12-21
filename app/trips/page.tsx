1b4cc88c5852cc1aa5a7fe02d82f6247c2811e23
basilspice <basil.breton@gmail.com>
2023-12-21 14:40:34 -0800

    started tripsbutton will save for another day

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,6 +1,6 @@
-
 import EmptyState from "@/app/components/EmptyState";
 import ClientOnly from "@/app/components/ClientOnly";
+
 
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getReservations from "@/app/actions/getReservations";
@@ -10,13 +10,11 @@ import TripsClient from "./TripsClient";
 const TripsPage = async () => {
   const currentUser = await getCurrentUser();
 
+
   if (!currentUser) {
     return (
       <ClientOnly>
-        <EmptyState
-          title="Unauthorized"
-          subtitle="Please login"
-        />
+        <EmptyState title="Unauthorized" subtitle="Please login" />
       </ClientOnly>
     );
   }
@@ -28,20 +26,18 @@ const TripsPage = async () => {
       <ClientOnly>
         <EmptyState
           title="No trips found"
-          subtitle="Looks like you havent reserved any trips."
+          subtitle="Looks like you havent reserved anything."
         />
+        
       </ClientOnly>
     );
   }
 
   return (
     <ClientOnly>
-      <TripsClient
-        reservations={reservations}
-        currentUser={currentUser}
-      />
+      <TripsClient reservations={reservations} currentUser={currentUser} />
     </ClientOnly>
   );
-}
- 
+};
+
 export default TripsPage;
