59530b4e136cdef524b590cc8d9cba3fc3272e51
basilspice <basil.breton@gmail.com>
2023-12-22 13:27:05 -0800

    made disabledDate optional. using DatePicker on searchmodal

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { Range } from "react-date-range";
-import DatePicker from "../inputs/DatePicker";
+
 import Button from "../Button";
+import DatePicker from "../inputs/DatePicker";
 
 interface ListingReservationProps {
   price: number;
@@ -21,11 +23,24 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
   onSubmit,
   disabled,
   disabledDates,
-}) => {
-  return (
-    <div className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden">
-      <div className="flex flex-row items-center gap-1 p-4">
-        <div className="text-2xl font-semibold">${price}</div>
+}) =>
+
+{
+  return ( 
+    <div
+      className="
+      bg-white 
+        rounded-xl 
+        border-[1px]
+      border-neutral-200 
+        overflow-hidden
+      "
+    >
+      <div
+        className="
+      flex flex-row items-center gap-1 p-4"
+      >
+        <div className="text-2xl font-semibold">$ {price}</div>
         <div className="font-light text-neutral-600">night</div>
       </div>
       <hr />
@@ -34,16 +49,28 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
         disabledDates={disabledDates}
         onChange={(value) => onChangeDate(value.selection)}
       />
-
+      
       <hr />
       <div className="p-4">
         <Button disabled={disabled} label="Reserve" onClick={onSubmit} />
       </div>
-      <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+      <hr />
+      <div
+        className="
+          p-4 
+          flex 
+          flex-row 
+          items-center 
+          justify-between
+          font-semibold
+          text-lg
+        "
+      >
         <div>Total</div>
         <div>$ {totalPrice}</div>
       </div>
     </div>
+   
   );
 };
 
